5ee950a4dad7f2fd8215d8326b8d38058941bbf0
ninhorata <paul@constell.com>
2023-01-16 18:10:45 +0100

    First commit setup

---
+++ app/src/main/java/com/example/todolist/Login.java
@@ -0,0 +1,67 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+
+public class Login extends AppCompatActivity {
+    Button loginBtn;
+    TextView registerBtn;
+    EditText emailText, passwordText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize the variables from the view
+        emailText = findViewById(R.id.emailBox);
+        passwordText = findViewById(R.id.passwordBox);
+
+        // Log in method
+        loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(view -> mAuth.signInWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    new Intent(Login.this, MainActivity.class);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(Login.this, "Authentication failed.",
+                        Toast.LENGTH_LONG).show();
+                }
+            }
+        ));
+
+        // Registering a new user
+        registerBtn = findViewById(R.id.registerLink);
+        registerBtn.setOnClickListener(view ->
+                mAuth.createUserWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(Login.this, "Registered", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }));
+    }
+}
---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.todolist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.todolist.db.TaskContract;
+import com.example.todolist.db.TaskDbHelper;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView mTaskListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTaskListView = (ListView) findViewById(R.id.list_todo);
+
+        updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Toast.makeText(this, "Registered", Toast.LENGTH_LONG).show();
+
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+
+                                updateUI();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+                return true;
+            case R.id.logout:
+                startActivity(new Intent(MainActivity.this, Login.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+
+    }
+}
