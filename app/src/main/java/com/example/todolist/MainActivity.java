26a36e47d1f59821f6e8138b6b3d8ef2ddad4681
ninhorata <paul@constell.com>
2023-01-16 20:17:30 +0100

    Added firestore and auth

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.todolist;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,19 +16,37 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.todolist.db.TaskContract;
-import com.example.todolist.db.TaskDbHelper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mTaskListView;
+    ArrayAdapter<String> mAdapterTasks;
+    private String userEmail;
+    private List<String> todos = new ArrayList<>();
+    private List<String> todosId = new ArrayList<>();
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // start Firebase Firestore and Auth
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        // get user email
+        userEmail = mAuth.getCurrentUser().getEmail();
         mTaskListView = (ListView) findViewById(R.id.list_todo);
 
         updateUI();
@@ -57,8 +72,14 @@ public class MainActivity extends AppCompatActivity {
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                String task = String.valueOf(taskEditText.getText());
+                                // add task to db
+                                String taskValue = taskEditText.getText().toString();
 
+                                Map<String, String> task = new HashMap<>();
+                                task.put("description", taskValue);
+                                task.put("user_email", userEmail);
+
+                                db.collection("todos").add(task);
                                 updateUI();
                             }
                         })
@@ -67,7 +88,9 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
                 return true;
             case R.id.logout:
-                startActivity(new Intent(MainActivity.this, Login.class));
+                mAuth.signOut();
+                onBackPressed();
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -83,7 +106,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateUI() {
-        ArrayList<String> taskList = new ArrayList<>();
+        db.collection("todos")
+            .whereEqualTo("user_email", userEmail)
+            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot value,
+                                    @Nullable FirebaseFirestoreException e) {
+                    if (e != null) {
+                        return;
+                    }
 
+                    todos.clear();
+                    todosId.clear();
+
+                    for (QueryDocumentSnapshot doc : value) {
+                        if (doc.get("description") != null) {
+                            todos.add(doc.getString("description"));
+                            todosId.add(doc.getId());
+                        }
+
+                        if(todos.size() == 0) {
+                            mTaskListView.setAdapter(null);
+                        } else {
+                            mAdapterTasks = new ArrayAdapter<String>(MainActivity.this, R.layout.item_todo, R.id.task_title, todos);
+                            mTaskListView.setAdapter(mAdapterTasks);
+                        }
+                    }
+                }
+            });
     }
 }
